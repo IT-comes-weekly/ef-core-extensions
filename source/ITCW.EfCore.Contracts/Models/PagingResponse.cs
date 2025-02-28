68922ab51a412d75e4cbd37ad78ece51360248b3
Adrian Wilp <adrian.kroeger@sva.de>
2025-02-28 19:38:57 +0100

    doc(models): pagingResponse

--- source/ITCW.EfCore.Contracts/Models/PagingResponse.cs
+++ source/ITCW.EfCore.Contracts/Models/PagingResponse.cs
@@ -1,8 +1,18 @@
 namespace ITCW.EfCore.Contracts.Models;
 
+/// <summary>
+/// POCO for storing paged responses.
+/// </summary>
+/// <typeparam name="T">Type of the paged data.</typeparam>
 public class PagingResponse<T>
 {
+    /// <summary>
+    /// The returned data.
+    /// </summary>
     public IEnumerable<T> Data { get; set; } = [];
 
+    /// <summary>
+    /// The total amount of data entries.
+    /// </summary>
     public int Count { get; set; }
 }
